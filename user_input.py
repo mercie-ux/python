287e2ae539eff9ac4e30d06b3a6b7499ca1c6838
mercie-ux <mbaomercy0@gmail.com>
2024-11-01 12:40:42 +0300

    initial

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# ask for user name
+user_name = input("What is your name?:  ")
+#ask for user's age
+Age = input("How old are you?" )
+# ask user for location
+location = input("Where do you live?" )
+# output of personalized message
+print(f"Hello {user_name}, you are {Age} years old and live in {location}.")
